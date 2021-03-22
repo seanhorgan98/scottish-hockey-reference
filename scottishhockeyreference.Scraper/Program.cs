5f1e89898c6de49bd336b00641673ea1373241ae
Sean Horgan <seanhorgan98@gmail.com>
2021-03-22 17:48:54 +0000

    SHR-31: Created method to scrape all leagues from SH.

--- scottishhockeyreference.Scraper/Program.cs
+++ scottishhockeyreference.Scraper/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace scottishhockeyreference.Scraper
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly string leagueURL = "https://www.scottish-hockey.org.uk/league-standings/";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await PrintLeagues();
+        }
+
+        static async Task PrintLeagues()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(leagueURL);
+            var AllLeagues = document.QuerySelectorAll("h2.text-uppercase");
+            List<string> result = new List<string>();
+
+            foreach (var item in AllLeagues)
+            {
+                Console.WriteLine(item.TextContent);
+            }
         }
     }
 }
